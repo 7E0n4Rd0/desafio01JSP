9cada93b6a56f1b8c6c50b7e88a91372f47f285c
leonArch <leomeireles66@gmail.com>
2024-12-17 09:03:30 -0300

    Project Created

---
+++ src/main/java/com/leonardo/desafio01JSP/Desafio01JspApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.desafio01JSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio01JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio01JspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/desafio01JSP/Desafio01JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.desafio01JSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio01JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
